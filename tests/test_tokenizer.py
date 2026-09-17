63b8b78db11a82380b74e49c740414d283144a9a
GGBond-WH <zcl02192021@gmail.com>
2026-09-17 11:26:48 +0800

    Revise bug

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,19 @@
+from week01_text_pipeline.text_pipeline.tokenizer import CharTokenizer
+
+s = "fhsetsfse. "
+unknow_str = "🤩"
+text = "abcdefghijkkkllllmnopqrstuvwxyz. "
+tokenizer = CharTokenizer(text=text)
+
+
+def test_decode_encode():
+    assert s == tokenizer.decode(tokenizer.encode(s))
+
+
+def test_vocab_size():
+    assert tokenizer.vocab_size == len(set(text)) + 1
+
+
+def test_unknown_str():
+    assert tokenizer.encode(unknow_str) == [tokenizer.vocab_size]
+    assert tokenizer.decode([tokenizer.vocab_size + 1]) == "\ufffd"
---
+++ week01_text_pipeline/text_pipeline/tokenizer.py
@@ -0,0 +1,60 @@
+class CharTokenizer:
+    def __init__(self, text: str) -> None:
+        self.text = text
+        chars = sorted(set(text))
+        self.vocab = {}
+        self.re_vocab = {}
+        for i, char in enumerate(chars):
+            self.vocab[char] = i
+            self.re_vocab[i] = char
+        self.vocab["\ufffd"] = len(self.vocab)
+        self.re_vocab[len(self.vocab)] = "\ufffd"
+
+    @property
+    def vocab_size(
+        self,
+    ) -> int:
+        return len(self.vocab)
+
+    def encode(self, s: str) -> list[int]:
+        """
+        编码字符串，转换为id列表。
+
+        遇到vocab里面没有的字符，统一id为：len(self.vocab)
+
+        Args:
+        参数1：需要进行编码的字符串s
+
+        Returns：
+        返回编码后的id列表
+
+        """
+        encode_list = []
+        for char in s:
+            if char not in self.vocab:
+                encode_list.append(self.vocab_size)
+            else:
+                encode_list.append(self.vocab[char])
+        return encode_list
+
+    def decode(self, ids: list[int]) -> str:
+        """
+        对ids列表展开解码，转换为原字符串。
+
+        遇到id为len(self.vocab) + 1的id统一解码为：\ufffd
+
+        Args:
+        参数1：id列表
+
+        Returns：
+        解码后的字符串
+        """
+        decode_str = ""
+        for id in ids:
+            tmp_str_len = len(decode_str)
+            for k, v in self.vocab.items():
+                if v == id:
+                    decode_str += k
+            if tmp_str_len == len(decode_str):
+                decode_str += "\ufffd"
+        return decode_str
